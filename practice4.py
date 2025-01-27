ad98a03e1ce30ed3d93c89b957cb7bc8b850b9d4
Waguteru <156518095+Waguteru@users.noreply.github.com>
2025-01-27 14:31:36 +0300

    Add files via upload

---
+++ practice4.py
@@ -0,0 +1,8 @@
+n = int(input("Введите значение: "))
+
+sum_of_factorials = 1
+curr_factorial = 1
+for i in range(2, n + 1):
+    curr_factorial *= i
+    sum_of_factorials += curr_factorial
+print(sum_of_factorials)
